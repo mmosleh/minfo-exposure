27b1b6f69f4772a6781355e6c0647631d86d9711
Mohsen Mosleh <mohsen.mosleh@gmail.com>
2022-05-16 18:02:19 +0100

    Add files via upload

--- R/misinfo_exposure_score.R
+++ R/misinfo_exposure_score.R
@@ -1,5 +1,5 @@
 #' Misinformation-Exposure Score Function
-#' This function calculates the a misinformaiton exposure score based on Twitter account that a user follows
+#' This function calculates the a misinformation exposure score based on Twitter account that a user follows
 #' @name get_misinfo_exposure_score
 #' @param user_id or screen_name of users for whom you want to calculate the scores
 #' @return users and their misinformation-exposure scores
@@ -7,11 +7,15 @@
 
 data(falsity.scores)
 
+
 get_misinfo_exposure_score <- function(users){
   friends <- rtweet::get_friends(users,verbose = TRUE)
-  friends_score <- merge(x=friends,y=falsity_score,by.x="user_id",by.y='elite_id_str',all.x=TRUE)
-  r <- aggregate(friends_score$falsity,by=list(friends_score$user),FUN=mean,na.rm=TRUE,na.action=NULL)
+  friends_score <- merge(x=friends,y=falsity_score,by.x="user_id",by.y='elite_id_str',all.x=TRUE, retryonratelimit = TRUE)
+  friends_score$falsity_score_weighted <- friends_score$falsity_score * friends_score$elite_weight
+  r <- aggregate(friends_score$falsity_score_weighted,by=list(friends_score$user),FUN=mean,na.rm=TRUE,na.action=NULL)
   colnames(r) <- c('user','misinfo_score')
   return(r)
 }
 
+
+get_misinfo_exposure_score(c('dg_rand'))
